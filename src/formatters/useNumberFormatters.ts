a1544b328275d8944939b38033dc1fbeac32082b
damianof <damianof@users.noreply.github.com>
2022-10-09 19:18:43 -0400

    additional helpers

--- src/formatters/useNumberFormatters.ts
+++ src/formatters/useNumberFormatters.ts
@@ -2,6 +2,21 @@
 
 const defaultcurrencyDisplay = 'symbol' // 'symbol', 'narrowSymbol', 'code', 'name'
 
+type NumberSeparators = {
+  decimal: string
+  group: string
+}
+
+const getNumberSeparators = (lcid: string): NumberSeparators => {
+  const numberWithSeparators = 1231.1
+  const parts = Intl.NumberFormat(lcid).formatToParts(numberWithSeparators)
+
+  return {
+    decimal: parts.find((part) => part.type === 'decimal')?.value as string,
+    group: parts.find((part) => part.type === 'group')?.value as string
+  }
+}
+
 // helper to calculate the cache key for the datetime Intl.NumberFormat instances
 export const getNumberFormattersCacheKey = (params: {
   lcid: string
@@ -75,6 +90,30 @@ export const useNumberFormatters = (localeId: string) => {
     return _cache.get(cacheKey) as Intl.NumberFormat
   }
 
+  const _privateUnformatFn = (inputValue: string): number => {
+    if (_cachedNumberSeparators) {
+      _cachedNumberSeparators = getNumberSeparators(_lcid)
+    }
+
+    const numberSeparators = _cachedNumberSeparators as NumberSeparators
+
+    const { decimal } = numberSeparators
+    console.log(`[^0-9\${ group }]`)
+    // Build regex to strip out everything except digits, decimal point and minus sign:
+    const regex = new RegExp(`[^0-9\\${decimal}]`, 'g')
+    const unformatted = parseFloat(
+      `${inputValue}`
+        .replace(/\((?=\d+)(.*)\)/, '-$1') // replace bracketed values with negatives
+        .replace(regex, '') // strip out any cruft
+        .replace(decimal, '.') // make sure decimal point is standard
+    )
+
+    // This will fail silently which may cause trouble, let's wait and see:
+    return !isNaN(unformatted) ? unformatted : 0
+  }
+
+  let _cachedNumberSeparators: NumberSeparators | undefined
+
   return {
     whole: () => {
       return _privateGetFormatter({
@@ -112,8 +151,19 @@ export const useNumberFormatters = (localeId: string) => {
       })
     },
 
+    numberSeparators: (): NumberSeparators => {
+      if (_cachedNumberSeparators) {
+        _cachedNumberSeparators = getNumberSeparators(_lcid)
+      }
+      return _cachedNumberSeparators as NumberSeparators
+    },
+
+    unformat: _privateUnformatFn,
+
     unescapeResult(result: string) {
       return (result || '').replace(/\xa0/g, ' ').replace(/\u202f/g, ' ')
     }
   }
 }
+
+export type UseNumberFormatters = ReturnType<typeof useNumberFormatters>
